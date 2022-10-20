8e0a43df26eb4725de886334f13c8dbb803af71e
Suenny Souza <suenny.s@gmail.com>
2022-10-20 11:50:23 -0300

    Criação das máscaras para os inputs parte 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
 function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
@@ -17,3 +19,15 @@ function setCardType(type) {
 //setCardType("mastercard")
 
 globalThis.setCardType = setCardType //tornou global
+
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "DD{/}YYYY",
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
